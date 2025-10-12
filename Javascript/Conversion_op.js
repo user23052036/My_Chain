b348a0086d80307b50393e8d82649cf82482f48d
Souvik Mandal <user23052036@gmail.com>
2025-10-13 01:18:00 +0530

    added some new files on converts in Javascript

---
+++ Javascript/Conversion_op.js
@@ -0,0 +1,26 @@
+let score = ""
+let valueInNumber = Number(score);
+
+console.log(typeof score)  
+console.log(typeof valueInNumber) 
+console.log(score) 
+console.log(valueInNumber) 
+
+/**
+ * 23 => 23
+ * 23abc => NaN
+ * royja => NaN
+ * "" => 0
+ */
+
+let loggedIn = ""
+let booleanLoggedIn = Boolean(loggedIn);
+console.log(loggedIn)
+console.log(booleanLoggedIn)
+
+/**
+ * 1 => true , 0 => false
+ * "" => false "dhjs" => true
+ */
+
+
